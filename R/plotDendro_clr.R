2d71ed2c301b3a12d19a52a183e4629127b97cad
Shraddha Pai <shraddha.pai@utoronto.ca>
2017-05-17 09:25:36 -0400

    explicitly add colnames to clrmat otherwise sample order gets mixed up

--- R/plotDendro_clr.R
+++ R/plotDendro_clr.R
@@ -6,7 +6,9 @@
 #' should contain column names for groupBy. Note that groupBy columns 
 #' should be factors
 #' @param groupPal (list) keys are columns in pheno by which data are to
-#' be grouped. Values are RColorBrewer palette names
+#' be grouped. Values could be RColorBrewer palette names (char length 1)
+#' or a vector of colours to use as the palette. In this case
+#' the palette will follow the order of the variable levels
 #' @param verbose (logical) print messages
 #' @param ... parameters for squash::dendromat
 #' @importFrom squash dendromat
@@ -22,21 +24,26 @@ plotDendro_clr <- function(M, pheno, groupPal,topVar=10*1000L, verbose=TRUE,
 	ctr <- 1
 	for (gpCol in names(groupPal)) {
 		gpLev 	<- levels(pheno[,gpCol])
-		pal		<- suppressWarnings(
-			RColorBrewer::brewer.pal(
-				name=groupPal[[gpCol]],n=max(3,length(gpLev)))
-		)
+		if (length(groupPal[[gpCol]])==1) { # palette name
+			pal		<- suppressWarnings(
+				RColorBrewer::brewer.pal(
+					name=groupPal[[gpCol]],n=max(3,length(gpLev)))
+			)
+		} else {
+			pal <- groupPal[[gpCol]]
+		}
 	clrmat[ctr,]	<- pal[as.integer(pheno[,gpCol])]
 	ctr <- ctr+1
 	}
 	clrmat <- clrmat[nrow(clrmat):1,]
+	colnames(clrmat) <- colnames(M)
 
 	var 	<- getVariance(M)
 	M_top	<- M[order(var,decreasing=TRUE)[1:topVar],]
 	d <- as.dist(1-cor(M_top))
 	dendro	<- hclust(d,method="average")
 
-	par(mar=c(6,6,4,3))
+	par(mar=c(6,4,4,1),las=1)
 	squash::dendromat(dendro,t(clrmat),...)
 	title(sprintf("%s: top %i\n",ttl,topVar))
 }
