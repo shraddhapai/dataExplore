99b1368570ca0738335ba55e8857fbc74ace40c1
shraddhapai <shraddha.pai@utoronto.ca>
2019-05-29 13:01:20 -0400

    more functionality

--- R/drawQQplot.R
+++ R/drawQQplot.R
@@ -18,7 +18,7 @@
 drawQQplot <- function(pvector,main="title",...) {
   o = -log10(sort(pvector,decreasing=F))
   e = -log10( 1:length(o)/length(o) )
-    plot(e,o,pch=19,cex=1, main=main, ...,
+    plot(e,o,pch=19,main=main, ...,
         xlab=expression(Expected~~-log[10](italic(p))),
         ylab=expression(Observed~~-log[10](italic(p))),
         xlim=c(0,max(e)), ylim=c(0,max(o)))
--- R/pcaColored.R
+++ R/pcaColored.R
@@ -4,11 +4,21 @@
 #' @param mdata (data.frame) metadata. rownames(mdata)=colnames(m)
 #' @param groupToPlot (char) vector of metadata variables
 #' using which to colour-code samples
+#' @param plotType (char, full or point) if "full" plots sample name. If 
+#' "point", simply plots a dot
+#' @param bufType (numeric) buffer around the points, by which to extend x/y
+#' lim. Increase this value to allow space to see the full sample name.
+#' @param legendCex (numeric) cex for legend
+#' @param center,scale (logical) options to center/scale matrix before PCA
+#'	See prcomp()
+#' @param pal (char) RColorBrewer2 palette name
 #' @import RColorBrewer
 #' @return No value. side effect of plotting pca graph
 #' @export
-pcaColored <- function(m,mdata,groupToPlot,cex=0.5,...) {
-	pr <- prcomp(t(m))
+pcaColored <- function(m,mdata,groupToPlot,cex=0.5,plotType="full",
+	bufSpace=0.01,legendCex=0.8,center=TRUE,scale=TRUE,
+	pal="Dark2",...) {
+	pr <- prcomp(t(m),center=center,scale=scale)
 
 	for (currGroup in groupToPlot) {
 			cat(sprintf("%s ****\n", currGroup))
@@ -17,27 +27,28 @@ pcaColored <- function(m,mdata,groupToPlot,cex=0.5,...) {
 		if (class(cur) %in% c("character","integer")) cur <- as.factor(cur)
 		lv <- levels(cur)
 		print(lv)
-		pal <- brewer.pal(n=length(lv), name="Dark2")
+		pal <- suppressWarnings(brewer.pal(n=length(lv), name=pal))
 
-		bf1 <- max(pr$x[,1])-min(pr$x[,1])*0.05
-		bf2 <- max(pr$x[,2])-min(pr$x[,2])*0.05
-		bf3 <- max(pr$x[,3])-min(pr$x[,3])*0.05
+		bf1 <- (max(pr$x[,1])-min(pr$x[,1]))*bufSpace
+		bf2 <- (max(pr$x[,2])-min(pr$x[,2]))*bufSpace
+		bf3 <- (max(pr$x[,3])-min(pr$x[,3]))*bufSpace
 		clrs <- pal[as.integer(cur)]
 
 		par(mfrow=c(2,2),mar=c(4,4,1,1))
-		for (plotType in c("full")) {
+		#for (plotType in c("full")) {
 		# prob could be more succinctly written with pairs()
 		plot(pr$x[,1],pr$x[,2],type='n',xlab='PC1',ylab='PC2',
 			 xlim=c(min(pr$x[,1])-bf1,max(pr$x[,1])+bf1),
 			 ylim=c(min(pr$x[,2])-bf1,max(pr$x[,2])+bf2),...)
 		if (plotType %in% "full") 
 			text(pr$x[,1],pr$x[,2],colnames(m),col=clrs,cex=cex)
-		else 
-			points(pr$x[,1],pr$x[,2],colnames(m),col=clrs,cex=cex,
+		else  {
+			points(pr$x[,1],pr$x[,2],col=clrs,cex=cex,
 				   pch=16)
+		}
 
 		legend("topright",fill=NA,border=NA,pch=16,col=pal,
-			   legend=lv,cex=0.5)
+			   legend=lv,cex=legendCex)
 		title(sprintf("%s",currGroup))
 
 		plot(pr$x[,2],pr$x[,3],type='n',xlab='PC2',ylab='PC3',
@@ -45,28 +56,34 @@ pcaColored <- function(m,mdata,groupToPlot,cex=0.5,...) {
 			 ylim=c(min(pr$x[,3])-bf3,max(pr$x[,3])+bf3),...)
 		if (plotType %in% "full") 
 			text(pr$x[,2],pr$x[,3],colnames(m),col=clrs,cex=cex)
-		else
-			points(pr$x[,2],pr$x[,3],colnames(m),col=clrs,cex=cex,
+		else {
+			points(pr$x[,2],pr$x[,3],col=clrs,cex=cex,
 				   pch=16)
+		}
 
 		plot(pr$x[,1],pr$x[,3],type='n',xlab='PC1',ylab='PC3',
 			 xlim=c(min(pr$x[,1])-bf1,max(pr$x[,1])+bf1),
 			 ylim=c(min(pr$x[,3])-bf3,max(pr$x[,3])+bf3),...)
 		if (plotType %in% "full")
 			text(pr$x[,1],pr$x[,3],colnames(m),col=clrs,cex=cex)
-		else
-			points(pr$x[,1],pr$x[,3],colnames(m),col=clrs,cex=cex,
-				   pch=16)
-
-		plot(pr$x[,2],pr$x[,3],type='n',xlab='PC2',ylab='PC3',
-			 xlim=c(min(pr$x[,2])-bf2,max(pr$x[,2])+bf2),
-			 ylim=c(min(pr$x[,3])-bf3,max(pr$x[,3])+bf3),...)
-		if (plotType %in% "full")
-			text(pr$x[,2],pr$x[,3],colnames(m),col=clrs,cex=cex)
-		else 
-			points(pr$x[,2],pr$x[,3],colnames(m),col=clrs,cex=cex,
+		else {
+			points(pr$x[,1],pr$x[,3],col=clrs,cex=cex,
 				   pch=16)
 		}
+
+		# plot pct variance explained by each PC
+		var <- pr$sdev^2
+		cat("Variance by PC:\n")
+		print((var[1:10]/sum(var))*100)
+		pct_var <- (cumsum(var)/sum(var))*100
+		barplot(pct_var[1:10],ylim=c(0,100),
+			main="% variance explained (cum)",
+			ylab="% var (cum)")
+		cat("% Variance explained by top 10 PCs\n")
+		print(round(pct_var[1:10]))
+		cat("\n")
 	}
+
+return(pr)
 	
 }
